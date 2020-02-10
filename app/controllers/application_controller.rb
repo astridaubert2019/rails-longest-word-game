b7e8ac9414e3cab8d93c3e63b0d06e22d49c425b
astrid aubert <astridaubert@gmail.com>
2020-02-10 20:52:59 +0100

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
+require 'open-uri'
+require 'json'
+
 class ApplicationController < ActionController::Base
+  def new
+    o = [('A'..'Z')].map(&:to_a).flatten
+    @letters = (0...10).map { o[rand(o.length)] }
+  end
+
+  def score
+    @guess = params[:word]
+    @letters = params[:letters]
+    @include = included?(@guess, @letters)
+    @english = english_word?(@guess)
+  end
+
+  def included?(word, grid)
+    word.chars.all? { |letter| word.count(letter) <= grid.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
 end
